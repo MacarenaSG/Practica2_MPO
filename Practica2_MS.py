6a0995762ca7ae2f99175f4ae67c56d84c633da6
MacarenaSG <macarena.silvagarcia@gmail.com>
2025-11-02 14:02:42 +0100

    V1.Enlace a repositorio

---
+++ Practica2_MS.py
@@ -0,0 +1,13 @@
+
+def mostrar_menu():
+    """Muestra las opciones disponibles del programa."""
+    print("\n================ MENÚ PRINCIPAL ================")
+    print("1) Listar contenido del directorio actual")
+    print("2) Crear un nuevo directorio")
+    print("3) Crear un archivo de texto")
+    print("4) Escribir texto en un archivo existente (añadir al final)")
+    print("5) Eliminar un archivo o directorio")
+    print("6) Mostrar información de un archivo o directorio")
+    print("7) Comentarios para Jordi")
+    print("8) Salir")
+    print("================================================")
